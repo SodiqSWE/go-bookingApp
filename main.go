c4a29c714ceed63dddbac51294f69e6edc9d522e
netrunner2 <aodeyale@choosefreedomit.com>
2024-01-20 13:30:53 -0500

    added a helper file

--- main.go
+++ main.go
@@ -75,13 +75,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
